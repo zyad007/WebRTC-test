71ccd4340d44839fd25d50ff0afb59e8b5c2e85b
Zyad Abdul-Nasser <zizo_007@live.com>
2024-02-17 15:26:16 +0200

    zzz

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -63,7 +63,7 @@ function updateUserList(socketIds) {
   });
 }
 
-const socket = io.connect("localhost:5000");
+const socket = io.connect("https://webrtc-test-l40m.onrender.com/");
 
 socket.on("update-user-list", ({ users }) => {
   updateUserList(users);
